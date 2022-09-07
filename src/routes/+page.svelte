44f99420fa00339840162419de22b5c4ec322749
fabiogiolito <fabiogiolito@gmail.com>
2022-09-07 20:58:10 +0100

    generate QR code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import QRCode from "qrcode-svg";
+
+  let address = "http://expa.com/";
+
+  $: svg = address ? new QRCode(address).svg() : '';
+  
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center space-y-10">
+
+  <input
+    type="text" bind:value={address}
+    class="w-[258px] border py-2 px-3 rounded-lg shadow"
+  />
+
+  <span class="border rounded-lg overflow-hidden">
+    {@html svg}
+  </span>
+
+</div>
